b6d78ad4d437da30c57c85610a871d3a4be2ac92
ss2261 <ss2261@kent.ac.uk>
2021-04-10 15:57:19 +0100

    pyramid - Perfect right triangle coded! But not quite a pyramid set

--- src/pyramid.java
+++ src/pyramid.java
@@ -18,11 +18,13 @@ public class pyramid {
         String star = "*";
         System.out.println("Pyramid pattern");
 
-        for (int i = 0; i <5; i++) {
-            System.out.println(star);
-            for (int j = 0; j < i + 1; j++) {
-                System.out.print(star);
+        for (int i = 0; i <4; i++) {
+            System.out.println(" "+star);
+            for (int j = 0; j <=i; j++) {
+                System.out.print(" "+star+"  ");
+
             }
         }
+        System.out.print(" "+star);
     }
 }

ef003eab9a854d8906ed474128de2170924edd8c
ss2261 <ss2261@kent.ac.uk>
2021-04-10 15:42:52 +0100

    pyramid - i have created some sort of right triangle pattern

--- src/pyramid.java
+++ src/pyramid.java
@@ -1,14 +1,28 @@
 /*pattern to created:
-*
-*          *
-          * *
-         * * *
-        * * * *
-       * * * * *
+ *
+ *          *
+ * *
+ * * *
+ * * * *
+ * * * * *
  */
 
 public class pyramid {
     public static void main(String[] args) {
+        pyramid();
+    }
 
+
+    public static void pyramid() {
+
+        String star = "*";
+        System.out.println("Pyramid pattern");
+
+        for (int i = 0; i <5; i++) {
+            System.out.println(star);
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(star);
+            }
+        }
     }
 }
